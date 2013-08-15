13657bcfb5d7468226046859a21c15527d5a3480
Hank Wang <drapho@gmail.com>
2013-08-15 14:23:11 +0800

    Merge drywall/master

--- utilities/workflow.js
+++ utilities/workflow.js
@@ -2,26 +2,26 @@
 
 exports = module.exports = function(req, res) {
   var workflow = new (require('events').EventEmitter)();
-
+  
   workflow.outcome = {
     success: false,
     errors: [],
     errfor: {}
   };
-
+  
   workflow.hasErrors = function() {
     return Object.keys(workflow.outcome.errfor).length !== 0 || workflow.outcome.errors.length !== 0;
   };
-
+  
   workflow.on('exception', function(err) {
     workflow.outcome.errors.push('Exception: '+ err);
     return workflow.emit('response');
   });
-
+  
   workflow.on('response', function() {
     workflow.outcome.success = !workflow.hasErrors();
     res.send(workflow.outcome);
   });
-
+  
   return workflow;
 };
