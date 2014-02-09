b3ea780ccfa080e65fe6df8258c486cb67979cac
jedireza <reza@akhavan.me>
2014-02-09 12:14:52 -0800

    better exception debugging

--- views/http/index.js
+++ views/http/index.js
@@ -12,11 +12,17 @@ exports.http404 = function(req, res){
 
 exports.http500 = function(err, req, res, next){
   res.status(500);
+
+  var data = { err: {} };
+  if (req.app.get('env') === 'development') {
+    data.err = err;
+    console.log(err.stack);
+  }
+
   if (req.xhr) {
-    res.send({ error: 'Something went wrong.' });
+    res.send({ error: 'Something went wrong.', details: data });
   }
   else {
-    console.log(err.stack);
-    res.render('http/500');
+    res.render('http/500', data);
   }
 };
