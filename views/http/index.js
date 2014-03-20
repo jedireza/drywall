10af2c458e792e0ac503a535f3141d81e08c8bbc
Precious Jahlom Agboado <japrag@outlook.com>
2014-03-20 13:18:28 +0000

    added 404dmin route handler for 404admin route
    added 404dmin route handler for 404admin route

--- views/http/index.js
+++ views/http/index.js
@@ -10,6 +10,16 @@ exports.http404 = function(req, res){
   }
 };
 
+exports.http404Admin = function(req, res){
+  res.status(404);
+  if (req.xhr) {
+    res.send({ error: 'Resource not found.' });
+  }
+  else {
+    res.render('http/404admin');
+  }
+};
+
 exports.http500 = function(err, req, res, next){
   res.status(500);
 
