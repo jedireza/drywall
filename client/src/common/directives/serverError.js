71a1b0108822bc070f8a4c86a93ea5bf5125c6c6
arthur <arthurckao@gmail.com>
2015-01-29 18:11:05 +0800

    * define angular directive to handle server side form validation error

---
+++ client/src/common/directives/serverError.js
@@ -0,0 +1,17 @@
+// A simple directive to handle server side form validation
+// http://codetunes.com/2013/server-form-validation-with-angular/
+angular.module('directives.serverError', [])
+  .directive('serverError', [ function () {
+    return {
+      restrict: 'A',
+      require: '?ngModel',
+      replace: true,
+      link: function (scope, element, attrs, ctrl) {
+        element.on('change', function(){
+          scope.$apply(function(){
+            ctrl.$setValidity('server', true);
+          })
+        });
+      }
+    };
+  }]);
