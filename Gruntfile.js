331d3453784e27a25bb07b7b7840569be7c18c2f
arthur <arthurckao@gmail.com>
2015-04-22 18:09:21 +0800

    um-74 - update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,46 +19,6 @@ module.exports = function(grunt) {
       vendor: {
         files: [
           {
-            expand: true, cwd: 'bower_components/bootstrap/',
-            src: ['js/**', 'less/**'], dest: 'public/vendor/bootstrap/'
-          },
-          {
-            expand: true, cwd: 'bower_components/backbone/',
-            src: ['backbone.js'], dest: 'public/vendor/backbone/'
-          },
-          {
-            expand: true, cwd: 'bower_components/font-awesome/',
-            src: ['fonts/**', 'less/**'], dest: 'public/vendor/font-awesome/'
-          },
-          {
-            expand: true, cwd: 'bower_components/html5shiv/dist/',
-            src: ['html5shiv.js'], dest: 'public/vendor/html5shiv/'
-          },
-          {
-            expand: true, cwd: 'bower_components/jquery/dist/',
-            src: ['jquery.js'], dest: 'public/vendor/jquery/'
-          },
-          {
-            expand: true, cwd: 'bower_components/jquery.cookie/',
-            src: ['jquery.cookie.js'], dest: 'public/vendor/jquery.cookie/'
-          },
-          {
-            expand: true, cwd: 'bower_components/momentjs/',
-            src: ['moment.js'], dest: 'public/vendor/momentjs/'
-          },
-          {
-            expand: true, cwd: 'bower_components/respond/src/',
-            src: ['respond.js'], dest: 'public/vendor/respond/'
-          },
-          {
-            expand: true, cwd: 'bower_components/underscore/',
-            src: ['underscore.js'], dest: 'public/vendor/underscore/'
-          }
-        ]
-      },
-      clientVendor: {
-        files: [
-          {
             expand: true, cwd: 'client/bower_components/jquery/dist/',
             src: ['jquery.js'], dest: '<%= distdir %>/vendor/'
           },
@@ -111,7 +71,7 @@ module.exports = function(grunt) {
             src: ['*.png', '*.gif', '*.jpg'], dest: '<%= distdir %>/img/'
           },
           {
-            expand: true, cwd: 'bower_components/font-awesome/fonts/',
+            expand: true, cwd: 'client/bower_components/font-awesome/fonts/',
             src: ['*'], dest: '<%= distdir %>/fonts/'
           }
         ]
@@ -184,7 +144,7 @@ module.exports = function(grunt) {
         options: {
           ignore: [
             'node_modules/**',
-            'public/**'
+            'client/**'
           ],
           ext: 'js'
         }
@@ -207,78 +167,9 @@ module.exports = function(grunt) {
         files: ['client/src/assets/sass/**/*.scss'],
         tasks: ['sass:dev']
       },
-      clientJS: {
-         files: [
-          'public/layouts/**/*.js', '!public/layouts/**/*.min.js',
-          'public/views/**/*.js', '!public/views/**/*.min.js'
-         ],
-         tasks: ['newer:uglify', 'newer:jshint:client']
-      },
       serverJS: {
-         files: ['views/**/*.js'],
-         tasks: ['newer:jshint:server']
-      },
-      clientLess: {
-         files: [
-          'public/layouts/**/*.less',
-          'public/views/**/*.less',
-          'public/less/**/*.less'
-         ],
-         tasks: ['newer:less']
-      },
-      layoutLess: {
-        files: [
-          'public/layouts/**/*.less',
-          'public/less/**/*.less'
-        ],
-        tasks: ['less:layouts']
-      }
-    },
-    uglify: {
-      options: {
-        sourceMap: true,
-        sourceMapName: function(filePath) {
-          return filePath + '.map';
-        }
-      },
-      layouts: {
-        files: {
-          'public/layouts/core.min.js': [
-            'public/vendor/jquery/jquery.js',
-            'public/vendor/jquery.cookie/jquery.cookie.js',
-            'public/vendor/underscore/underscore.js',
-            'public/vendor/backbone/backbone.js',
-            'public/vendor/bootstrap/js/affix.js',
-            'public/vendor/bootstrap/js/alert.js',
-            'public/vendor/bootstrap/js/button.js',
-            'public/vendor/bootstrap/js/carousel.js',
-            'public/vendor/bootstrap/js/collapse.js',
-            'public/vendor/bootstrap/js/dropdown.js',
-            'public/vendor/bootstrap/js/modal.js',
-            'public/vendor/bootstrap/js/tooltip.js',
-            'public/vendor/bootstrap/js/popover.js',
-            'public/vendor/bootstrap/js/scrollspy.js',
-            'public/vendor/bootstrap/js/tab.js',
-            'public/vendor/bootstrap/js/transition.js',
-            'public/vendor/momentjs/moment.js',
-            'public/layouts/core.js'
-          ],
-          'public/layouts/ie-sucks.min.js': [
-            'public/vendor/html5shiv/html5shiv.js',
-            'public/vendor/respond/respond.js',
-            'public/layouts/ie-sucks.js'
-          ],
-          'public/layouts/admin.min.js': ['public/layouts/admin.js']
-        }
-      },
-      views: {
-        files: [{
-          expand: true,
-          cwd: 'public/views/',
-          src: ['**/*.js', '!**/*.min.js'],
-          dest: 'public/views/',
-          ext: '.min.js'
-        }]
+        files: ['service/**/*.js'],
+        task: ['newer:jshint:server']
       }
     },
     jshint: {
@@ -294,76 +185,20 @@ module.exports = function(grunt) {
           'client/src/common/**/*.js'
         ]
       },
-      //client: {
-      //  options: {
-      //    jshintrc: '.jshintrc-client',
-      //    ignores: [
-      //      'public/layouts/**/*.min.js',
-      //      'public/views/**/*.min.js'
-      //    ]
-      //  },
-      //  src: [
-      //    'public/layouts/**/*.js',
-      //    'public/views/**/*.js'
-      //  ]
-      //},
       server: {
         options: {
           jshintrc: '.jshintrc-server'
         },
         src: [
           'schema/**/*.js',
-          'views/**/*.js'
+          'service/**/*.js'
         ]
-      }
-    },
-    less: {
-      options: {
-        compress: true
-      },
-      layouts: {
-        files: {
-          'public/layouts/core.min.css': [
-            'public/less/bootstrap-build.less',
-            'public/less/font-awesome-build.less',
-            'public/layouts/core.less'
-          ],
-          'public/layouts/admin.min.css': ['public/layouts/admin.less']
-        }
-      },
-      views: {
-        files: [{
-          expand: true,
-          cwd: 'public/views/',
-          src: ['**/*.less'],
-          dest: 'public/views/',
-          ext: '.min.css'
-        }]
       }
     },
     clean: {
-      client: {
-        src: [
-            'client/dist/**'
-        ]
-      },
-      js: {
-        src: [
-          'public/layouts/**/*.min.js',
-          'public/layouts/**/*.min.js.map',
-          'public/views/**/*.min.js',
-          'public/views/**/*.min.js.map'
-        ]
-      },
-      css: {
-        src: [
-          'public/layouts/**/*.min.css',
-          'public/views/**/*.min.css'
-        ]
-      },
-      vendor: {
-        src: ['public/vendor/**']
-      }
+      src: [
+        'client/dist/**'
+      ]
     },
     useminPrepare: {
       html: '<%= distdir %>/index.html',
@@ -381,7 +216,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
@@ -392,15 +226,15 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-html2js');
   grunt.loadNpmTasks('grunt-karma');
 
-  grunt.registerTask('default', ['dev']);
 
-  grunt.registerTask('front', ['copy:clientVendor', 'copy:asset', 'copy:index', 'html2js', 'concat:angular', 'sass:dev']);
-  grunt.registerTask('back', ['copy:vendor', 'newer:uglify', 'newer:less']);
+  grunt.registerTask('angular', ['copy', 'html2js', 'concat:angular', 'sass:dev']);
 
   grunt.registerTask('lint', ['jshint']);
-  grunt.registerTask('unitTest', ['karma:unit']);
-
+  grunt.registerTask('unitTest', ['clean', 'angular', 'karma:unit']);
   grunt.registerTask('test', ['clean', 'lint']);
-  grunt.registerTask('dev', ['clean', 'front', 'back', 'concurrent']);
-  grunt.registerTask('production', ['clean', 'front', 'back', 'useminPrepare', 'concat:generated', 'uglify:generated', 'cssmin:generated', 'usemin']);
+
+  grunt.registerTask('dev', ['clean', 'angular', 'concurrent']);
+  grunt.registerTask('production', ['clean', 'angular', 'useminPrepare', 'concat:generated', 'uglify:generated', 'cssmin:generated', 'usemin']);
+
+  grunt.registerTask('default', ['dev']);
 };
