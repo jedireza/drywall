d59573e8d2091ed54e3bf35d7b288534f8ccb840
arthur <arthurckao@gmail.com>
2015-04-22 14:42:02 +0800

    um-74 - refactor /service/account.js

--- routes.js
+++ routes.js
@@ -14,34 +14,6 @@ function useAngular(req, res, next){
   res.sendFile(require('path').join(__dirname, './client/dist/index.html'));
 }
 
-function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  res.set('X-Auth-Required', 'true');
-  req.session.returnUrl = req.originalUrl;
-  res.redirect('/login/');
-}
-
-function ensureAdmin(req, res, next) {
-  if (req.user.canPlayRoleOf('admin')) {
-    return next();
-  }
-  res.redirect('/');
-}
-
-function ensureAccount(req, res, next) {
-  if (req.user.canPlayRoleOf('account')) {
-    if (req.app.config.requireAccountVerification) {
-      if (req.user.roles.account.isVerified !== 'yes' && !/^\/account\/verification\//.test(req.url)) {
-        return res.redirect('/account/verification/');
-      }
-    }
-    return next();
-  }
-  res.redirect('/');
-}
-
 function apiEnsureAuthenticated(req, res, next){
   if(req.isAuthenticated()){
     return next();
@@ -95,14 +67,17 @@ exports = module.exports = function(app, passport) {
   //-----authentication required api-----
   app.all('/api/account*', apiEnsureAuthenticated);
   app.all('/api/account*', apiEnsureAccount);
-  app.all('/api/account/settings*', apiEnsureVerifiedAccount);
-  app.get('/api/account/settings', account.getAccountDetails);
-  app.put('/api/account/settings', require('./views/account/settings/index').update);
-  app.put('/api/account/settings/identity', require('./views/account/settings/index').identity);
-  app.put('/api/account/settings/password', require('./views/account/settings/index').password);
+
   app.get('/api/account/verification', account.upsertVerification);
   app.post('/api/account/verification', account.resendVerification);
   app.get('/api/account/verification/:token/', account.verify);
+
+  app.all('/api/account/settings*', apiEnsureVerifiedAccount);
+
+  app.get('/api/account/settings', account.getAccountDetails);
+  app.put('/api/account/settings', account.update);
+  app.put('/api/account/settings/identity', account.identity);
+  app.put('/api/account/settings/password', account.password);
   app.get('/api/account/settings/google/callback', account.connectGoogle);
   app.get('/api/account/settings/google/disconnect', account.disconnectGoogle);
   app.get('/api/account/settings/facebook/callback', account.connectFacebook);
@@ -171,7 +146,6 @@ exports = module.exports = function(app, passport) {
 
   //admin > search
   app.get('/api/admin/search', admin.search);
-
 
   //******** END OF NEW JSON API ********
 
@@ -246,145 +220,4 @@ exports = module.exports = function(app, passport) {
   app.all(/^(?!\/api).*$/, useAngular);
 
   //******** End OF static routes ********
-
-  //front end
-  //app.get('/', require('./views/index').init);
-  //app.get('/about/', require('./views/about/index').init);
-  //app.get('/contact/', require('./views/contact/index').init);
-  //app.post('/contact/', require('./views/contact/index').sendMessage);
-
-  //sign up
-  //app.get('/signup/', require('./views/signup/index').init);
-  //app.post('/signup/', require('./views/signup/index').signup);
-
-  //social sign up
-  //app.post('/signup/social/', require('./views/signup/index').signupSocial);
-  //app.get('/signup/twitter/', passport.authenticate('twitter', { callbackURL: '/signup/twitter/callback/' }));
-  //app.get('/signup/twitter/callback/', require('./views/signup/index').signupTwitter);
-  //app.get('/signup/github/', passport.authenticate('github', { callbackURL: '/signup/github/callback/', scope: ['user:email'] }));
-  //app.get('/signup/github/callback/', require('./views/signup/index').signupGitHub);
-  //app.get('/signup/facebook/', passport.authenticate('facebook', { callbackURL: '/signup/facebook/callback/', scope: ['email'] }));
-  //app.get('/signup/facebook/callback/', require('./views/signup/index').signupFacebook);
-  //app.get('/signup/google/', passport.authenticate('google', { callbackURL: '/signup/google/callback/', scope: ['profile email'] }));
-  //app.get('/signup/google/callback/', require('./views/signup/index').signupGoogle);
-  //app.get('/signup/tumblr/', passport.authenticate('tumblr', { callbackURL: '/signup/tumblr/callback/' }));
-  //app.get('/signup/tumblr/callback/', require('./views/signup/index').signupTumblr);
-
-  //login/out
-  //app.get('/login/', require('./views/login/index').init);
-  //app.post('/login/', require('./views/login/index').login);
-  //app.get('/login/forgot/', require('./views/login/forgot/index').init);
-  //app.post('/login/forgot/', require('./views/login/forgot/index').send);
-  //app.get('/login/reset/', require('./views/login/reset/index').init);
-  //app.get('/login/reset/:email/:token/', require('./views/login/reset/index').init);
-  //app.put('/login/reset/:email/:token/', require('./views/login/reset/index').set);
-  //app.get('/logout/', require('./views/logout/index').init);
-
-  //social login
-  //app.get('/login/twitter/', passport.authenticate('twitter', { callbackURL: '/login/twitter/callback/' }));
-  //app.get('/login/twitter/callback/', require('./views/login/index').loginTwitter);
-  //app.get('/login/github/', passport.authenticate('github', { callbackURL: '/login/github/callback/' }));
-  //app.get('/login/github/callback/', require('./views/login/index').loginGitHub);
-  //app.get('/login/facebook/callback/', require('./views/login/index').loginFacebook);
-  //app.get('/login/google/callback/', require('./views/login/index').loginGoogle);
-  //app.get('/login/tumblr/', passport.authenticate('tumblr', { callbackURL: '/login/tumblr/callback/', scope: ['profile email'] }));
-  //app.get('/login/tumblr/callback/', require('./views/login/index').loginTumblr);
-
-  //admin
-  //app.all('/admin*', ensureAuthenticated);
-  //app.all('/admin*', ensureAdmin);
-  //app.get('/admin/', require('./views/admin/index').init);
-
-  //admin > users
-  //app.get('/admin/users/', require('./views/admin/users/index').find);
-  //app.post('/admin/users/', require('./views/admin/users/index').create);
-  //app.get('/admin/users/:id/', require('./views/admin/users/index').read);
-  //app.put('/admin/users/:id/', require('./views/admin/users/index').update);
-  //app.put('/admin/users/:id/password/', require('./views/admin/users/index').password);
-  //app.put('/admin/users/:id/role-admin/', require('./views/admin/users/index').linkAdmin);
-  //app.delete('/admin/users/:id/role-admin/', require('./views/admin/users/index').unlinkAdmin);
-  //app.put('/admin/users/:id/role-account/', require('./views/admin/users/index').linkAccount);
-  //app.delete('/admin/users/:id/role-account/', require('./views/admin/users/index').unlinkAccount);
-  //app.delete('/admin/users/:id/', require('./views/admin/users/index').delete);
-
-  //admin > administrators
-  //app.get('/admin/administrators/', require('./views/admin/administrators/index').find);
-  //app.post('/admin/administrators/', require('./views/admin/administrators/index').create);
-  //app.get('/admin/administrators/:id/', require('./views/admin/administrators/index').read);
-  //app.put('/admin/administrators/:id/', require('./views/admin/administrators/index').update);
-  //app.put('/admin/administrators/:id/permissions/', require('./views/admin/administrators/index').permissions);
-  //app.put('/admin/administrators/:id/groups/', require('./views/admin/administrators/index').groups);
-  //app.put('/admin/administrators/:id/user/', require('./views/admin/administrators/index').linkUser);
-  //app.delete('/admin/administrators/:id/user/', require('./views/admin/administrators/index').unlinkUser);
-  //app.delete('/admin/administrators/:id/', require('./views/admin/administrators/index').delete);
-
-  //admin > admin groups
-  //app.get('/admin/admin-groups/', require('./views/admin/admin-groups/index').find);
-  //app.post('/admin/admin-groups/', require('./views/admin/admin-groups/index').create);
-  //app.get('/admin/admin-groups/:id/', require('./views/admin/admin-groups/index').read);
-  //app.put('/admin/admin-groups/:id/', require('./views/admin/admin-groups/index').update);
-  //app.put('/admin/admin-groups/:id/permissions/', require('./views/admin/admin-groups/index').permissions);
-  //app.delete('/admin/admin-groups/:id/', require('./views/admin/admin-groups/index').delete);
-
-  //admin > accounts
-  //app.get('/admin/accounts/', require('./views/admin/accounts/index').find);
-  //app.post('/admin/accounts/', require('./views/admin/accounts/index').create);
-  //app.get('/admin/accounts/:id/', require('./views/admin/accounts/index').read);
-  //app.put('/admin/accounts/:id/', require('./views/admin/accounts/index').update);
-  //app.put('/admin/accounts/:id/user/', require('./views/admin/accounts/index').linkUser);
-  //app.delete('/admin/accounts/:id/user/', require('./views/admin/accounts/index').unlinkUser);
-  //app.post('/admin/accounts/:id/notes/', require('./views/admin/accounts/index').newNote);
-  //app.post('/admin/accounts/:id/status/', require('./views/admin/accounts/index').newStatus);
-  //app.delete('/admin/accounts/:id/', require('./views/admin/accounts/index').delete);
-
-  //admin > statuses
-  //app.get('/admin/statuses/', require('./views/admin/statuses/index').find);
-  //app.post('/admin/statuses/', require('./views/admin/statuses/index').create);
-  //app.get('/admin/statuses/:id/', require('./views/admin/statuses/index').read);
-  //app.put('/admin/statuses/:id/', require('./views/admin/statuses/index').update);
-  //app.delete('/admin/statuses/:id/', require('./views/admin/statuses/index').delete);
-
-  //admin > categories
-  //app.get('/admin/categories/', require('./views/admin/categories/index').find);
-  //app.post('/admin/categories/', require('./views/admin/categories/index').create);
-  //app.get('/admin/categories/:id/', require('./views/admin/categories/index').read);
-  //app.put('/admin/categories/:id/', require('./views/admin/categories/index').update);
-  //app.delete('/admin/categories/:id/', require('./views/admin/categories/index').delete);
-
-  //admin > search
-  //app.get('/admin/search/', require('./views/admin/search/index').find);
-
-  //account
-  //app.all('/account*', ensureAuthenticated);
-  //app.all('/account*', ensureAccount);
-  //app.get('/account/', require('./views/account/index').init);
-
-  //account > verification
-  //app.get('/account/verification/', require('./views/account/verification/index').init);
-  //app.post('/account/verification/', require('./views/account/verification/index').resendVerification);
-  //app.get('/account/verification/:token/', require('./views/account/verification/index').verify);
-  //
-  //account > settings
-  //app.get('/account/settings/', require('./views/account/settings/index').init);
-  //app.put('/account/settings/', require('./views/account/settings/index').update);
-  //app.put('/account/settings/identity/', require('./views/account/settings/index').identity);
-  //app.put('/account/settings/password/', require('./views/account/settings/index').password);
-
-  //account > settings > social
-  //app.get('/account/settings/twitter/', passport.authenticate('twitter', { callbackURL: '/account/settings/twitter/callback/' }));
-  //app.get('/account/settings/twitter/callback/', require('./views/account/settings/index').connectTwitter);
-  //app.get('/account/settings/twitter/disconnect/', require('./views/account/settings/index').disconnectTwitter);
-  //app.get('/account/settings/github/', passport.authenticate('github', { callbackURL: '/account/settings/github/callback/' }));
-  //app.get('/account/settings/github/callback/', require('./views/account/settings/index').connectGitHub);
-  //app.get('/account/settings/github/disconnect/', require('./views/account/settings/index').disconnectGitHub);
-  //app.get('/account/settings/facebook/callback/', require('./views/account/settings/index').connectFacebook);
-  //app.get('/account/settings/facebook/disconnect/', require('./views/account/settings/index').disconnectFacebook);
-  //app.get('/account/settings/google/callback/', require('./views/account/settings/index').connectGoogle);
-  //app.get('/account/settings/google/disconnect/', require('./views/account/settings/index').disconnectGoogle);
-  //app.get('/account/settings/tumblr/', passport.authenticate('tumblr', { callbackURL: '/account/settings/tumblr/callback/' }));
-  //app.get('/account/settings/tumblr/callback/', require('./views/account/settings/index').connectTumblr);
-  //app.get('/account/settings/tumblr/disconnect/', require('./views/account/settings/index').disconnectTumblr);
-
-  //route not found
-  //app.all('*', require('./views/http/index').http404);
 };
--- service/account.js
+++ service/account.js
@@ -147,6 +147,239 @@ var account = {
 
     require('async').parallel([getAccountData, getUserData], asyncFinally);
   },
+  update: function(req, res, next){
+    var workflow = req.app.utility.workflow(req, res);
+
+    workflow.on('validate', function() {
+      if (!req.body.first) {
+        workflow.outcome.errfor.first = 'required';
+      }
+
+      if (!req.body.last) {
+        workflow.outcome.errfor.last = 'required';
+      }
+
+      if (workflow.hasErrors()) {
+        return workflow.emit('response');
+      }
+
+      workflow.emit('patchAccount');
+    });
+
+    workflow.on('patchAccount', function() {
+      var fieldsToSet = {
+        name: {
+          first: req.body.first,
+          middle: req.body.middle,
+          last: req.body.last,
+          full: req.body.first +' '+ req.body.last
+        },
+        company: req.body.company,
+        phone: req.body.phone,
+        zip: req.body.zip,
+        search: [
+          req.body.first,
+          req.body.middle,
+          req.body.last,
+          req.body.company,
+          req.body.phone,
+          req.body.zip
+        ]
+      };
+      var options = { select: 'name company phone zip' };
+
+      req.app.db.models.Account.findByIdAndUpdate(req.user.roles.account.id, fieldsToSet, options, function(err, account) {
+        if (err) {
+          return workflow.emit('exception', err);
+        }
+
+        workflow.outcome.account = account;
+        return workflow.emit('response');
+      });
+    });
+
+    workflow.emit('validate');
+  },
+  identity: function(req, res, next){
+    var workflow = req.app.utility.workflow(req, res);
+
+    workflow.on('validate', function() {
+      if (!req.body.username) {
+        workflow.outcome.errfor.username = 'required';
+      }
+      else if (!/^[a-zA-Z0-9\-\_]+$/.test(req.body.username)) {
+        workflow.outcome.errfor.username = 'only use letters, numbers, \'-\', \'_\'';
+      }
+
+      if (!req.body.email) {
+        workflow.outcome.errfor.email = 'required';
+      }
+      else if (!/^[a-zA-Z0-9\-\_\.\+]+@[a-zA-Z0-9\-\_\.]+\.[a-zA-Z0-9\-\_]+$/.test(req.body.email)) {
+        workflow.outcome.errfor.email = 'invalid email format';
+      }
+
+      if (workflow.hasErrors()) {
+        return workflow.emit('response');
+      }
+
+      workflow.emit('duplicateUsernameCheck');
+    });
+
+    workflow.on('duplicateUsernameCheck', function() {
+      req.app.db.models.User.findOne({ username: req.body.username, _id: { $ne: req.user.id } }, function(err, user) {
+        if (err) {
+          return workflow.emit('exception', err);
+        }
+
+        if (user) {
+          workflow.outcome.errfor.username = 'username already taken';
+          return workflow.emit('response');
+        }
+
+        workflow.emit('duplicateEmailCheck');
+      });
+    });
+
+    workflow.on('duplicateEmailCheck', function() {
+      req.app.db.models.User.findOne({ email: req.body.email.toLowerCase(), _id: { $ne: req.user.id } }, function(err, user) {
+        if (err) {
+          return workflow.emit('exception', err);
+        }
+
+        if (user) {
+          workflow.outcome.errfor.email = 'email already taken';
+          return workflow.emit('response');
+        }
+
+        workflow.emit('patchUser');
+      });
+    });
+
+    workflow.on('patchUser', function() {
+      var fieldsToSet = {
+        username: req.body.username,
+        email: req.body.email.toLowerCase(),
+        search: [
+          req.body.username,
+          req.body.email
+        ]
+      };
+      var options = { select: 'username email twitter.id github.id facebook.id google.id' };
+
+      req.app.db.models.User.findByIdAndUpdate(req.user.id, fieldsToSet, options, function(err, user) {
+        if (err) {
+          return workflow.emit('exception', err);
+        }
+
+        workflow.emit('patchAdmin', user);
+      });
+    });
+
+    workflow.on('patchAdmin', function(user) {
+      if (user.roles.admin) {
+        var fieldsToSet = {
+          user: {
+            id: req.user.id,
+            name: user.username
+          }
+        };
+        req.app.db.models.Admin.findByIdAndUpdate(user.roles.admin, fieldsToSet, function(err, admin) {
+          if (err) {
+            return workflow.emit('exception', err);
+          }
+
+          workflow.emit('patchAccount', user);
+        });
+      }
+      else {
+        workflow.emit('patchAccount', user);
+      }
+    });
+
+    workflow.on('patchAccount', function(user) {
+      if (user.roles.account) {
+        var fieldsToSet = {
+          user: {
+            id: req.user.id,
+            name: user.username
+          }
+        };
+        req.app.db.models.Account.findByIdAndUpdate(user.roles.account, fieldsToSet, function(err, account) {
+          if (err) {
+            return workflow.emit('exception', err);
+          }
+
+          workflow.emit('populateRoles', user);
+        });
+      }
+      else {
+        workflow.emit('populateRoles', user);
+      }
+    });
+
+    workflow.on('populateRoles', function(user) {
+      user.populate('roles.admin roles.account', 'name.full', function(err, populatedUser) {
+        if (err) {
+          return workflow.emit('exception', err);
+        }
+
+        workflow.outcome.user = populatedUser;
+        workflow.emit('response');
+      });
+    });
+
+    workflow.emit('validate');
+  },
+  password: function(req, res, next){
+    var workflow = req.app.utility.workflow(req, res);
+
+    workflow.on('validate', function() {
+      if (!req.body.newPassword) {
+        workflow.outcome.errfor.newPassword = 'required';
+      }
+
+      if (!req.body.confirm) {
+        workflow.outcome.errfor.confirm = 'required';
+      }
+
+      if (req.body.newPassword !== req.body.confirm) {
+        workflow.outcome.errors.push('Passwords do not match.');
+      }
+
+      if (workflow.hasErrors()) {
+        return workflow.emit('response');
+      }
+
+      workflow.emit('patchUser');
+    });
+
+    workflow.on('patchUser', function() {
+      req.app.db.models.User.encryptPassword(req.body.newPassword, function(err, hash) {
+        if (err) {
+          return workflow.emit('exception', err);
+        }
+
+        var fieldsToSet = { password: hash };
+        req.app.db.models.User.findByIdAndUpdate(req.user.id, fieldsToSet, function(err, user) {
+          if (err) {
+            return workflow.emit('exception', err);
+          }
+
+          user.populate('roles.admin roles.account', 'name.full', function(err, user) {
+            if (err) {
+              return workflow.emit('exception', err);
+            }
+
+            workflow.outcome.newPassword = '';
+            workflow.outcome.confirm = '';
+            workflow.emit('response');
+          });
+        });
+      });
+    });
+
+    workflow.emit('validate');
+  },
   upsertVerification: function(req, res, next){
     var workflow = req.app.utility.workflow(req, res);
 
